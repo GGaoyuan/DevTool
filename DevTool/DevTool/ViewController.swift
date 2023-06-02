49b1b7d76c1e3b909e6f5576a4db2f7ec55ac74d
gaoyuan <707216098@qq.com>
2023-06-02 18:33:19 +0800

    add

---
+++ DevTool/DevTool/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DevTool
+//
+//  Created by gaoyuan on 2023/6/2.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
